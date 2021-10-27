2d64d371d0e8286e2992cb224406d2697e86db0e
Nathalie Benarroch <nbenarroch@jdlabs.co>
2021-10-26 21:50:14 -0400

    save done

--- pages/SaveAndShare.js
+++ pages/SaveAndShare.js
@@ -4,6 +4,7 @@ import {
   SafeAreaView,
   StyleSheet,
   View,
+  Image,
   Text,
   ScrollView,
   TouchableOpacity,
@@ -11,9 +12,47 @@ import {
 import Menu from "./Menu";
 import Logoutbutton from "./Logoutbutton";
 
-export default function SaveAndShare({ setPage }) {
-  const [outfits, setOutfits] = useState([]);
-  useEffect(() => {}, []);
+export default function SaveAndShare({ setPage, outfits, setOutfits }) {
+  const [renderOutfits, setRenderOutfits] = useState([]);
+  const [isShareModalOpen, setIsShareModalOpen] = useState(false);
+  const [indexOutfit, setIndexOutfit] = useState(0);
+  const [title, setTitle] = useState("");
+  useEffect(() => {
+    let val = indexOutfit;
+    if (!Number.isInteger(val)) {
+      return;
+    }
+
+    setTitle(outfits[indexOutfit]["title"]);
+  }, [indexOutfit]);
+  useEffect(() => {
+    let value = [];
+    if (outfits.length === 0) {
+      setTitle("no oufit created yet");
+    } else {
+      setTitle(outfits[0]["title"]);
+    }
+    outfits.forEach((outfit, index) => {
+      let tempValue = [
+        <View style={styles.pictureContainer} key={index}>
+          <Image
+            style={styles.picture}
+            source={{ uri: outfit["clothes"][0]["url"] }}
+          />
+          <Image
+            style={styles.picture}
+            source={{ uri: outfit["clothes"][1]["url"] }}
+          />
+          <Image
+            style={styles.picture}
+            source={{ uri: outfit["clothes"][2]["url"] }}
+          />
+        </View>,
+      ];
+      value.push(tempValue);
+    });
+    setRenderOutfits(value);
+  }, [outfits]);
   return (
     <>
       <View style={styles.container}>
@@ -28,22 +67,43 @@ export default function SaveAndShare({ setPage }) {
         <View style={styles.bordercards}>
           <ScrollView
             snapToInterval={400}
-            decelerationRate={0.85}
+            decelerationRate={0.95}
             horizontal={true}
             onScroll={(event) => {
-              setIndexClothesTop(event.nativeEvent.contentOffset.x / 400);
+              setIndexOutfit(event.nativeEvent.contentOffset.x / 400);
             }}
           >
-            {/* {renderClothesTop} */}
+            {renderOutfits}
           </ScrollView>
 
           <View style={styles.cardTitle}>
             {/* <Text style={styles.cardTitleText}>{category1}</Text> */}
-            <Text style={styles.cardTitleText}>HELLO</Text>
+            <Text style={styles.cardTitleText}>{title}</Text>
           </View>
         </View>
 
+        <TouchableOpacity
+          onPress={() => {
+            setIsShareModalOpen(true);
+          }}
+          style={styles.saveButton}
+        >
+          <Text style={styles.saveButtonTitle}>Share</Text>
+        </TouchableOpacity>
+
         <Menu setPage={setPage} />
+
+        {
+          isShareModalOpen === true && (
+            <View>LOL</View>
+            // <SaveModal
+            //   saveOutfit={saveOutfit}
+            //   setIsSaveModalOpen={setIsSaveModalOpen}
+            // />
+          )
+
+          // ABOVE IS PROP OR EXPORT THAT IS INITIATED HERE AND PASSED TO DAILY INSPIRATION CHILD COMPONENT
+        }
       </View>
     </>
   );
@@ -103,5 +163,42 @@ const styles = StyleSheet.create({
     fontSize: 15,
     fontWeight: "300",
     color: "white",
+  },
+
+  pictureContainer: {
+    paddingTop: 120,
+    width: 400,
+    height: 400,
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+  },
+  picture: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 150,
+    height: 150,
+    resizeMode: "contain",
+  },
+  saveButton: {
+    position: "absolute",
+    right: 0,
+    marginTop: "auto",
+    marginBottom: "auto",
+    backgroundColor: "white",
+    borderRadius: 15,
+    paddingRight: 10,
+    paddingLeft: 10,
+    borderWidth: 4,
+    borderColor: "black",
+    borderStyle: "solid",
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+
+  saveButtonTitle: {
+    fontSize: 15,
+    fontWeight: "300",
+    color: "black",
   },
 });
